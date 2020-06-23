08972347a2f5b1d64ed549e76731ce986b968313
Boris Brodski <brodsky_boris@yahoo.com>
2020-06-23 21:18:22 +0200

    Use 'PWD-' trigger

--- assets/javascripts/redmine-password-encrypter.js
+++ assets/javascripts/redmine-password-encrypter.js
@@ -3,81 +3,91 @@ $.PWD.password = null;
 
 
 function encrypt(msg, pwd) {
-	var wordArray = CryptoJS.enc.Utf8.parse(msg);  
-	var encrypted = CryptoJS.AES.encrypt(wordArray, pwd);
-	return encrypted.toString();
+    var wordArray = CryptoJS.enc.Utf8.parse(msg);  
+    var encrypted = CryptoJS.AES.encrypt(wordArray, pwd);
+    return encrypted.toString();
 }
 
 function decrypt(crypted, pwd) {
-	var decrypted = CryptoJS.AES.decrypt(crypted, pwd);
-	var str = decrypted.toString(CryptoJS.enc.Utf8);
-	if (str.length == 0) {
-	  return null;
-	}
-	return str;
+    try {
+        var decrypted = CryptoJS.AES.decrypt(crypted, pwd);
+        var str = decrypted.toString(CryptoJS.enc.Utf8);
+        if (str.length == 0) {
+          return null;
+        }
+        return str;
+    } catch (e) {
+        return null;
+    }
 }
 
 var selectedInput = null;
 $(function() {
-    $('input, textarea, select').focus(function() {
-        $.PWD.selectedInput = this;
-    }).blur(function(){
-        if ($.PWD.selectedInput != null) {
-			$.PWD.lastSelectedInput = $.PWD.selectedInput;
-        }
-        $.PWD.selectedInput = null;
-    });
+    $(document).keydown(function(event) {
+        src = event.srcElement;
+        if (src != null
+            && src.localName == "textarea"
+            && src == $.PWD.currentSrc
+            && event.key.length == 1) {
+            if ($.PWD.lastSequence == "PWD" && event.key == "-") {
+                while ($.PWD.password == null) {
+                    master_password = prompt("Enter master password", "");
+                    if (master_password == null) {
+                        return
+                    }
 
-    $(document).keyup(function(event) {
-        if ($.PWD.selectedInput != null) {
-            orig_text = $.PWD.selectedInput.value;
-            text = orig_text.replace(/PWD\(([^)\n]+)\)/g, function($0, $1) {
-                if ($.PWD.password == null) {
-                    $.PWD.password = prompt("Enter password", "");
-
-					match = /PWD-([-A-Za-z0-9+\/]+)/.exec(text);
-					if (match && decrypt(match[1], $.PWD.password) == null) {
-						alert("Incorrect password");
-						$.PWD.password = null;
-						return "PWD(" + $1;
-					}
+                    match = /PWD-([-A-Za-z0-9+\/]+)/.exec(src.value);
+                    if (match && decrypt(match[1], master_password) == null) {
+                        alert("Incorrect password");
+                    } else {
+                        $.PWD.password = master_password;
+                    }
+                } 
+                if ($.PWD.password != null && $.PWD.password.length > 0) {
+                    password = prompt("Enter password to encrypt", "");
+                    if (password != null) {
+                        encrypted_password = encrypt(password, $.PWD.password);
+                        document.execCommand('insertText', false /*no UI*/,
+                            "-" + encrypted_password);
+                    }
                 }
-				if ($.PWD.password.length > 0) {
-					return "PWD-" + encrypt($1, $.PWD.password);
-				}
-				return "PWD(" + $1;
-            });
-            
-            if (text != orig_text) {
-                start = $.PWD.selectedInput.selectionStart - $.PWD.selectedInput.value.length + text.length;
-                $.PWD.selectedInput.value = text;
-                $.PWD.selectedInput.setSelectionRange(start, start);
-            }
-        }
-    });
-    
-	decorateEncryptedPasswords($("div.wiki"));
 
-	$(document).ajaxComplete(function () {
-		decorateEncryptedPasswords($("div.wiki"));
-	});
+                $.PWD.lastSequence = "";
+
+                event.preventDefault();
+                return false;
+            } else {
+                $.PWD.lastSequence = $.PWD.lastSequence + event.key;
+                $.PWD.lastSequence = $.PWD.lastSequence.substring($.PWD.lastSequence.length - 3)
+            }
+        } else {
+            $.PWD.currentSrc = src;
+            $.PWD.lastSequence = "";
+        }
+    })
+    
+    decorateEncryptedPasswords($("div.wiki"));
+
+    $(document).ajaxComplete(function () {
+        decorateEncryptedPasswords($("div.wiki"));
+    });
     
     $("div.wiki").on('click', "span.encrypted-pwd", function () {
         crypted = $(this).text();
-		if ($.PWD.password == null) {
+        if ($.PWD.password == null) {
             $.PWD.password = prompt("Enter password", "");
         }
 
-		plain = decrypt(crypted, $.PWD.password);
+        plain = decrypt(crypted, $.PWD.password);
 
-		if (plain == null) {
+        if (plain == null) {
             alert('Invalid password');
-			$.PWD.password = null;
-		} else {
-			$(this).text(plain);
-			$(this).attr('class', '');
-			$(this).attr('title', 'Decrypted password. The password will be hidden next time you load this page.');
-		}
+            $.PWD.password = null;
+        } else {
+            $(this).text(plain);
+            $(this).attr('class', '');
+            $(this).attr('title', 'Decrypted password. The password will be hidden next time you load this page.');
+        }
     });
 });
 
